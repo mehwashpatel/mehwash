653fe1e84d5387b201ebf3cf0b4ab1ac4cc25ecb
Mehwash Patel <mehwashpatel@Mehwashs-MacBook-Pro.local>
2020-04-12 17:02:29 -0400

    Updates

---
+++ precache-manifest.afd0ed4f13199cdd19fa2e741bcf5882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90da6e44eb67db8afbbba88adb327932",
+    "url": "/mehwash/index.html"
+  },
+  {
+    "revision": "5ca5ecbc8031d12c7454",
+    "url": "/mehwash/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb8ad143aa9774ea76d8",
+    "url": "/mehwash/static/js/2.7e6faea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mehwash/static/js/2.7e6faea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca5ecbc8031d12c7454",
+    "url": "/mehwash/static/js/main.deeb5984.chunk.js"
+  },
+  {
+    "revision": "8eb107ce91dc7ac9b6c3",
+    "url": "/mehwash/static/js/runtime-main.a2d42de6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mehwash/static/media/logo.5d5d9eef.svg"
+  }
+]);
